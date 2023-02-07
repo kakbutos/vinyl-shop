50d56ca521e33b730f7296ffe1911a11e54f2e2f
Popova Natalya <popova.natashar@gmail.com>
2023-02-07 14:19:04 +0200

    Вывод категорий на главной странице

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -3,15 +3,22 @@
 namespace Eshop\Controllers;
 
 use Eshop\Core\Template\Template;
+use Eshop\src\Repositories\TagRepository;
+use Exception;
 
 class MainController
 {
+	/**
+	 * @throws Exception
+	 */
 	public function mainAction(): void
 	{
+		$tags = (new TagRepository())->getList();
+
 		$render = new Template('../src/Views');
 		print $render->render('layout', [
 			'header' => $render->render('/components/header', []),
-			'sidebar' => $render->render('/components/sidebar', []),
+			'sidebar' => $render->render('/components/sidebar', ['tags' => $tags]),
 			'pagination' => $render->render('/components/pagination', []),
 			'mainPage' => $render->render('/public/main', []),
 			]);
