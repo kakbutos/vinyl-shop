7bb5529d26f9394d7eeb387bfd26a18983c7dca9
Popova Natalya <popova.natashar@gmail.com>
2023-02-25 16:40:08 +0200

    Скрипт для корзины

---
+++ public/js/cart_script.js
@@ -0,0 +1,80 @@
+$('.buy-button').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).data('id');
+	$.ajax({
+		url: '/cart/add/' + id +'/',
+		type: 'GET',
+		success: function(result){
+			if (!result) alert('Продукта с таким id не существует.')
+			showMessage(result);
+		},
+		error: function(){
+			alert('Не удалось добавить товар в корзину.')
+		}
+	})
+});
+
+function showMessage() {
+	var modal = document.getElementById("modal");
+	var span = document.getElementsByClassName("close")[0];
+	modal.style.display = "block";
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+	window.onclick = function(event) {
+		if (event.target === modal)
+		{
+			modal.style.display = "none"
+		}
+	}
+	let quantity = document.getElementById('quantity').textContent;
+	let newQuantity = Number(quantity) + 1;
+	$('.products-quantity').html(newQuantity);
+}
+
+$('.incr-count-button').on('click', function(e) {
+	let id = e.target.id;
+	let count = document.getElementById("product-count" + id).value;
+	console.log(count);
+	let price = document.getElementById('price' +id).textContent;
+	price = Number(price.replace(/[a-zа-яё]/gi, ''));
+	$('#sum' + id).html(count*price + ' руб');
+	$.ajax({
+		url: '/cart/add/' + id +'/',
+		type: 'GET',
+		success: function(result){
+			if (!result) alert('Продукта с таким id не существует.')
+		},
+		error: function(){
+			alert('Не удалось добавить товар в корзину.')
+		}
+	})
+
+	let quantity = document.getElementById('quantity').textContent;
+	let newQuantity = Number(quantity) + 1;
+	$('.products-quantity').html(newQuantity);
+});
+
+$('.decr-count-button').on('click', function(e) {
+	let id = e.target.id;
+	let count = document.getElementById("product-count" + id).value;
+	let price = document.getElementById('price' +id).textContent;
+	price = Number(price.replace(/[a-zа-яё]/gi, ''));
+	$('#sum' + id).html(count*price + ' руб');
+	$.ajax({
+		url: '/cart/reduce/' + id +'/',
+		type: 'GET',
+		success: function(result){
+			if (!result) alert('Продукта с таким id не существует.')
+		},
+		error: function(){
+			alert('Не удалось уменьшить количество товара в корзине')
+		}
+	})
+
+	let quantity = document.getElementById('quantity').textContent;
+	if (quantity > 1) {
+		let newQuantity = Number(quantity) - 1;
+		$('.products-quantity').html(newQuantity);
+	}
+});
